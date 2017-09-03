5f3430ce8ca927ae5b73f572477124dd15d9dc00
Canale <raffaele.canale@epfl.ch>
2017-09-03 22:30:39 +0200

    Restructure project + add compiled scripts + config

--- setUp.sh
+++ setUp.sh
@@ -3,15 +3,19 @@
 function printHelp() {
 cat << EOF
 
+    IMPORTANT: Be sure to setup the script config at `echoGreen $CONFIG_FILE`
+    An example config file can be found at ${CONFIG_FILE}.example
+
     Usage: ${PROGNAME}
 
     Create a new .bashrc file with the template at `echoBlue "<dir>/Others/.bashrc_template"`
-    and the appropriate PATH variable
+    and the appropriate PATH variable.
 EOF
 }
 
 function declareConstants() {
-    return 0
+    readonly COMPILED_PATH_DIR="./path.c"
+    readonly CONFIG_FILE="./data/config.properties"
 }
 
 function loadOptions() {
@@ -23,14 +27,38 @@ function sedeasy {
   sed "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
 }
 
-function run () {
-    bashrcFile="./Others/.bashrc_template"
+function exportBashrc() {
+    bashrcFile="./lib/.bashrc_template"
 
     test -f "$bashrcFile" || error "$bashrcFile file not found"
 
     sedeasy "__PATH__" "$PWD" $bashrcFile > "$HOME/.bashrc"
+
+    source $HOME/.bashrc
+}
+
+function compileScripts() {
+    applyProperties="./path/applyProperties" # Uncompiled version
+
+    test -d "$COMPILED_PATH_DIR" || mkdir "$COMPILED_PATH_DIR" || error "Failed to create $COMPILED_PATH_DIR"
+    test -f "$CONFIG_FILE" || error "Config file $CONFIG_FILE not found"
+
+
+    for script in "./path/"*; do
+        compiled="$COMPILED_PATH_DIR/$(basename "$script")"
+
+        cat "$script" | $applyProperties "$CONFIG_FILE" > "$compiled"
+        chmod +x "$compiled"
+    done
+}
+
+function run() {
+    set +o nounset
+
+    exportBashrc
+    compileScripts
 }
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-source "./Others/scriptStarter"
+source "./lib/scriptStarter"
